2bb28e64900b3874adf1658558ebf1941946bf97
Ankur Auti <115859079+Webdevava@users.noreply.github.com>
2024-03-31 11:21:25 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,6 +10,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZS6ZB7LYJC"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZS6ZB7LYJC');
+</script>
       <body className="dark">
       <Layout>
       {children}
